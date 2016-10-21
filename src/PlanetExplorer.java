cd6d6a70decf6677caf6d88c2f2c1029970a36e1
jkorpeli <jkorpeli@STUPID-PC728.stnet.local>
2016-10-21 16:42:05 +0300

    finished other than obstacles not added

--- src/PlanetExplorer.java
+++ src/PlanetExplorer.java
@@ -4,6 +4,10 @@
 // Finish time:
 
 public class PlanetExplorer {
+	private String position = "(0,0,N)";
+	private int maxOfX = 0;
+	private int maxOfY = 0;
+	
 	public PlanetExplorer(int x, int y, String obstacles){
 	/*	x and y represent the size of the grid.
 	 *  Obstacles is a String formatted as follows: "(obs1_x,obs1_y)(obs2_x,obs2_y)...(obsN_x,obsN_y)" with no white spaces. 
@@ -12,6 +16,9 @@ public class PlanetExplorer {
 		PlanetExplorer explorer = new PlanetExplorer(100,100,"(5,5)(7,8)")  
 		 
 	 */
+		maxOfX = x - 1;
+		maxOfY= y - 1;
+		
 	}
 	
 	public String executeCommand(String command){
@@ -25,7 +32,104 @@ public class PlanetExplorer {
 		 * Where pos_x and pos_y are the final coordinates, facing is the current direction the explorer is pointing to (N,S,W,E).
 		 * The return string should also contain a list of coordinates of the encountered obstacles. No white spaces.
 		 */
+		char directions[] = "NESW".toCharArray();
+		int direction = 0;
 		
-		return null;
+		for(int i=0; i < command.length(); i++){
+		
+			int x_coord = Character.getNumericValue(position.charAt(1));
+			int y_coord = Character.getNumericValue(position.charAt(3));
+			
+			if( command.charAt(i) == 'f'){
+				if(direction == 0){
+					if(y_coord == maxOfY){
+						y_coord = 0;
+					}else{
+					y_coord += 1;
+					}
+				}else if(direction == 1){
+					if(x_coord == maxOfX){
+						x_coord = 0;
+					}else{
+						x_coord +=1;
+					}
+				}else if(direction == 2){
+					if(y_coord == 0){
+						y_coord = maxOfY;
+					}else{
+						y_coord -=1;
+					}
+				}else{
+					if(x_coord == 0){
+						x_coord = maxOfX;
+					}else{
+						x_coord -=1;
+					}
+				}
+				
+				String newPosition = String.format("(%d,%d,%s)",x_coord,y_coord, Character.toString(directions[direction]));
+				
+				setPosition(newPosition);
+			}else if(command.charAt(i) == 'b'){
+				
+				if(direction == 0){
+					if(y_coord == 0){
+						y_coord = maxOfY;
+					}else{
+					y_coord -= 1;
+					}
+				}else if(direction == 1){
+					if(x_coord == 0){
+						x_coord = maxOfX;
+					}else{
+						x_coord -=1;
+					}
+				}else if(direction == 2){
+					if(y_coord == maxOfY){
+						y_coord = 0;
+					}else{
+						y_coord +=1;
+					}
+				}else{
+					if(x_coord == maxOfX){
+						x_coord = 0;
+					}else{
+						x_coord +=1;
+					}
+				}
+				
+				String newPosition = String.format("(%d,%d,%s)",x_coord,y_coord, Character.toString(directions[direction]));
+				
+				setPosition(newPosition);
+			}else if(command.charAt(i) == 'r'){
+				direction += 1;
+				if(direction == 4){
+					direction = 0;
+				}
+				
+				String newDir = Character.toString(directions[direction]);
+				String newPosition = position.substring(0, 5) + newDir + position.substring(6, 7);
+				
+				setPosition(newPosition);	
+			}else if(command.charAt(i) == 'l'){
+				direction -= 1;
+				if(direction == -1){
+					direction = 3;
+				}
+				String newDir = Character.toString(directions[direction]);
+				String newPosition = position.substring(0, 5) + newDir + position.substring(6, 7);
+				
+				setPosition(newPosition);
+			}
+		}
+		return position;
+	}
+	
+	public String getPosition(){
+		return position;
+	}
+	
+	public void setPosition(String newPos){
+		position = newPos;
 	}
 }
--- tests/TestPlanetExplorer.java
+++ tests/TestPlanetExplorer.java
@@ -5,7 +5,75 @@ import org.junit.Test;
 public class TestPlanetExplorer {
 
 	@Test
-	public void test_executeCommand() {
-		fail("Not yet implemented");
+	public void test_CreatePlanetExplorer() {
+		PlanetExplorer explorer = new PlanetExplorer(0, 0, "");
+		
+		assertNotNull(explorer);
+	}
+	
+	@Test
+	public void test_getExplorerPositionAtStart(){
+		PlanetExplorer explorer = new PlanetExplorer(10,10,"");
+		
+		assertEquals("not getting position",explorer.getPosition(), "(0,0,N)");
+	}
+	
+	@Test
+	public void test_SetExplorerPosition(){
+		PlanetExplorer explorer = new PlanetExplorer(10,10,"");
+		
+		explorer.setPosition("(2,2,W)");
+		
+		assertEquals("not setting position",explorer.getPosition(), "(2,2,W)");
+	}
+	
+	@Test
+	public void test_Moving1SpaceForwards(){
+		PlanetExplorer explorer = new PlanetExplorer(10,10,"");
+		
+		assertEquals("not moving forwards","(0,1,N)", explorer.executeCommand("f"));
+		
+	}
+	
+	@Test
+	public void test_changingDirection(){
+		PlanetExplorer explorer = new PlanetExplorer(10,10,"");
+		
+		assertEquals("not changing direction","(0,0,E)", explorer.executeCommand("r"));
+	}
+	
+	@Test
+	public void test_executeffrf(){
+		PlanetExplorer explorer = new PlanetExplorer(10,10,"");
+		
+		assertEquals("not going following direction","(1,2,E)", explorer.executeCommand("ffrf"));
+	}
+	
+	@Test
+	public void test_executeffbrf(){
+		PlanetExplorer explorer = new PlanetExplorer(10,10,"");
+		
+		assertEquals("not going back properly","(1,1,E)", explorer.executeCommand("ffbrf"));
+	}
+	
+	@Test
+	public void test_executerfflf(){
+		PlanetExplorer explorer = new PlanetExplorer(10,10,"");
+		
+		assertEquals("testing turning left","(2,1,N)", explorer.executeCommand("rfflf"));
+	}
+	
+	@Test
+	public void test_executefrfrfrfrfr(){
+		PlanetExplorer explorer = new PlanetExplorer(10,10,"");
+		
+		assertEquals("going out of bounds of the direction array","(0,1,E)", explorer.executeCommand("frfrfrfrfr"));
+	}
+	
+	@Test
+	public void test_executelf(){
+		PlanetExplorer explorer = new PlanetExplorer(3,3,"");
+		
+		assertEquals("appearing on the otherside of the grid not working", "(2,0,W)", explorer.executeCommand("lf"));
 	}
 }
